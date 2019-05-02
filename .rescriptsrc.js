4d9a44466b0fe748b0ad33471053b8e27fd6ac6a
zbeyens <ziad.beyens@borealisgroup.com>
2019-05-02 13:27:17 +0200

    dev: minimum config for rescripts

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -14,43 +14,14 @@ module.exports = [
             },
           },
         ],
-        [
-          'import',
-          {
-            libraryName: 'antd',
-            style: true,
-          },
-        ],
+        // customize babel plugins
       ],
     },
   ],
   config => {
     const newConfig = config;
 
-    let rule = newConfig.module.rules.find(rule => rule.oneOf);
-
-    rule.oneOf.unshift({
-      test: /\.less$/,
-      use: [
-        {
-          loader: 'style-loader',
-        },
-        {
-          loader: 'css-loader',
-        },
-        {
-          loader: 'less-loader',
-          options: {
-            javascriptEnabled: true,
-            // https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
-            modifyVars: {
-              '@primary-color': '#005d9a',
-              '@link-color': '#0099d2',
-            },
-          },
-        },
-      ],
-    });
+    // customize webpack config
 
     return newConfig;
   },
