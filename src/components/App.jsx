50d7cdd843899b3dfe8a298010bbc791bebdaa1e
Lennert Van Sever <lennert.vansever@borealisgroup.com>
2019-05-02 13:31:44 +0200

    os-13 moving style config of app to wrapper

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,18 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import { Switch, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
 import store, { history } from 'store/createStore';
-import theme from 'theme/defaultTheme';
-import GlobalStyle from 'theme/globalStyle';
+import StyleWrapper from 'theme/StyleWrapper';
 import { HomePage } from 'components';
 
 const App = () => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyle />
-          <Switch>
-            <Route path="/" exact component={HomePage} />
-          </Switch>
-        </>
-      </ThemeProvider>
+      <StyleWrapper>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+        </Switch>
+      </StyleWrapper>
     </ConnectedRouter>
   </Provider>
 );
---
+++ src/theme/StyleWrapper.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import theme from 'theme/defaultTheme';
+import GlobalStyle from 'theme/globalStyle';
+
+const StyleWrapper = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <>
+      <GlobalStyle />
+      {children}
+    </>
+  </ThemeProvider>
+);
+
+export default StyleWrapper;
